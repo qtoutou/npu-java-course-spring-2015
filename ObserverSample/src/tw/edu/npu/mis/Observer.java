3c98f94cdea7904919d16266798fc05d743c8bf2
qtoutou <topgdb09@gmail.com>
2015-05-31 21:01:47 +0800

    delete

--- ObserverSample/src/tw/edu/npu/mis/Observer.java
+++ ObserverSample/src/tw/edu/npu/mis/Observer.java
@@ -32,7 +32,7 @@ package tw.edu.npu.mis;
 class Observer {
 
     void update() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
